4cf060310e89e6501d2f13b7a35ccd544f910476
Valerij <shishov265@gmail.com>
2023-07-06 01:17:19 +0300

    add: list_tile_content in home_widgets

--- lib/pages/home/widgets/home_widgets.dart
+++ lib/pages/home/widgets/home_widgets.dart
@@ -1,3 +1,4 @@
 export 'package:yando/pages/home/widgets/create_task_tile.dart';
 export 'package:yando/pages/home/widgets/home_sliver_bar.dart';
 export 'package:yando/pages/home/widgets/list_tile.dart';
+export 'package:yando/pages/home/widgets/list_tile_content.dart';
